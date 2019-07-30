42f66aa441b609cc5aa86f50ede4d19c74e640ac
RFNotebook\radiofisik <mail@radiofisik.ru>
2019-07-30 22:11:37 +0200

    simple proxy pass

--- src/App/Auth/AuthServer/Program.cs
+++ src/App/Auth/AuthServer/Program.cs
@@ -21,6 +21,7 @@ namespace AuthServer
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://*:5005")
                 .UseCustomSerilog();
     }
 }
